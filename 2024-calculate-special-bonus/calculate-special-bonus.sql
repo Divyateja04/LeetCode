d44e17502cd5768926a9e7ada151395df4f4d04b
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2025-05-22 23:56:37 +0530

    Time: 602 ms (61.82%) | Memory: 0B (100.00%) - LeetSync

--- 2024-calculate-special-bonus/calculate-special-bonus.sql
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -1,3 +1,5 @@
-    select employee_id , salary * ( employee_id%2 ) * ( name not like 'M%') as bonus
-    from employees
-    order by employee_id;
+select 
+    employee_id,
+    if(employee_id%2!=0 and name NOT LIKE 'M%',salary,0) as bonus 
+from Employees 
+order by employee_id
