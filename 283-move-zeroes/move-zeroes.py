3f0700f7f4181bbbe3b85abd21d027ed0289ff51
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2025-05-24 23:47:42 +0530

    Time: 7 ms (60.78%) | Memory: 13.6 MB (35.75%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        zero = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i],nums[zero] =nums[zero],nums[i]
+                zero+=1
+        
