81a986fa8788b582801f0c34bd4af6d2fc58bbd3
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2025-06-07 18:55:00 +0530

    Time: 1199 ms (75.00%) | Memory: 24.8 MB (75.00%) - LeetSync

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
@@ -0,0 +1,17 @@
+class Solution:
+    def clearStars(self,s):
+        heap = []
+        for i,c in enumerate(s):
+            if c=='*':
+                heappop(heap)
+            else:
+                heappush(heap,(c,-i))
+        
+        ans = ['']*len(s)
+        while heap:
+            char,i = heappop(heap)
+            ans[-i] = char
+            
+        return ''.join(ans)
+            
+        
