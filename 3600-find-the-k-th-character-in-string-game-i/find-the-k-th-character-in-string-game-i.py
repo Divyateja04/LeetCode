f0ee13be018eba6945efbe0588dbd2c67cab72c5
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2025-07-03 21:07:16 +0530

    Time: 34 ms (10.00%) | Memory: 12.4 MB (82.00%) - LeetSync

---
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def kthCharacter(self, k):
+        sb = ['a']
+        while len(sb) < k:
+            size = len(sb)
+            for i in range(size):
+                next_char = chr(ord('a') + ((ord(sb[i]) - ord('a') + 1) % 26))
+                sb.append(next_char)
+        return sb[k - 1]
+        
