0b2cdbd6cde286bafd04545652b2903388ceee9f
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2025-08-25 20:28:43 +0530

    Time: 29 ms (22.17%) | Memory: 16 MB (17.58%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+class Solution(object):
+    def findDiagonalOrder(self, mat):
+        R, C = len(mat), len(mat[0])
+        diagonalDict = defaultdict(list)
+		# key - diagonal elements have the same r + c value.
+        for r in range(R):
+            for c in range(C):
+                diagonalDict[r+c].append(mat[r][c])
+        ans = []
+        for i, value in enumerate(diagonalDict.values()):
+            if i % 2 == 0:
+                ans += value[::-1]
+            else:
+                ans += value
+        return ans
+        
