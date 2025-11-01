2aedb519c79c04b06b2fb6501a0254c226e15e59
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2025-11-01 21:05:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (12.39%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getSneakyNumbers(self, nums):
+        seen = set()
+        r = []
+        for i in nums:
+            if i in seen:
+                r.append(i)
+            else:
+                seen.add(i)
+        return r
+        
