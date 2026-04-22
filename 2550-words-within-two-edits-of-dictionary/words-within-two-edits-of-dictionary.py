dc2290ddfdef539ccc7f9bf23f1d460385d2059a
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-04-22 22:47:53 +0530

    Time: 207 ms (33.33%) | Memory: 12.3 MB (91.67%) - LeetSync

---
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoEditWords(self, queries, dictionary):
+        r=[]
+        for q in queries:
+            for d in dictionary:
+                c=0
+                for i in range(len(q)):
+                    if q[i]!=d[i]:
+                        c+=1
+                if c<=2:
+                    r.append(q)
+                    break
+        return r
