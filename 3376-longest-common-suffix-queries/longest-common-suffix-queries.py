a181faacbaeb577eb3cf35fb05a7a66f993c4d04
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-05-28 22:19:37 +0530

    Time: 1902 ms (87.50%) | Memory: 209.9 MB (87.50%) - LeetSync

---
+++ 3376-longest-common-suffix-queries/longest-common-suffix-queries.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def stringIndices(self, wordsContainer, wordsQuery):
+        trie = {}
+        for i,word in enumerate(wordsContainer):
+            spot = trie
+            if None not in spot or spot[None][1] > len(word):
+                spot[None] = (i,len(word))
+            for c in reversed(word):
+                if c not in spot:
+                    spot[c] = {}
+                spot = spot[c]
+                if None not in spot or spot[None][1] > len(word):
+                    spot[None] = (i,len(word))
+        ans = []
+        for word in wordsQuery:
+            spot = trie
+            for c in reversed(word):
+                if c not in spot:
+                    break
+                spot = spot[c]
+            ans.append(spot[None][0])
+        return ans
